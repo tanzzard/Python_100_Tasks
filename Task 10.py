e59e3cc7d1b8389c0e1874183db7347d94bd275f
tanzzard <02230303.cst@rub.edu.bt>
2023-10-09 22:09:27 -0600

    First task upload

---
+++ Task 10.py
@@ -0,0 +1,4 @@
+age: int = 18
+print(age)
+name = "dorji"
+print("Name: ", name, "Age: ", age)
---
+++ Task 9.py
@@ -0,0 +1,5 @@
+age: int = 18
+print(age)
+name = "dorji"
+#The type of the variable name is a string(str) because it contains a string value.
+print(name)
